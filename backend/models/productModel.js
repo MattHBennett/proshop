0f50a23ff71cf1dfb0215235cfbc9b839e07ea24
Matthew H Bennett <matt.h.bennett@gmail.com>
2024-02-08 18:55:26 -0500

    Added a seeder to import and destroy data for MongoDB.

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const reviewShcema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema({
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true,
