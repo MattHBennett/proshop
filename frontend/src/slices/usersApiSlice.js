43c6d990cd7ea5b25eeb83720157b2c9ccd8251e
Matthew H Bennett <matt.h.bennett@gmail.com>
2024-02-26 00:56:22 -0500

    Added update user functionality.

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -43,8 +43,32 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				url: `${USERS_URL}/${userId}`,
 				method: "DELETE"
 			})
+		}),
+		getUserDetails: builder.query({
+			query: (userId) => ({
+				url: `${USERS_URL}/${userId}`,
+				method: "GET"
+			}),
+			keepUnusedDataFor: 5
+		}),
+		updateUser: builder.mutation({
+			query: (data) => ({
+				url: `${USERS_URL}/${data.userId}`,
+				method: "PUT",
+				body: data
+			}),
+			invalidatesTags: ["Users"]
 		})
 	}),
 });
 
-export const { useLoginMutation, useRegisterMutation, useLogoutMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation } = usersApiSlice;
+export const {
+	useLoginMutation,
+	useRegisterMutation,
+	useLogoutMutation,
+	useProfileMutation,
+	useGetUsersQuery,
+	useDeleteUserMutation,
+	useGetUserDetailsQuery, 
+	useUpdateUserMutation
+} = usersApiSlice;
